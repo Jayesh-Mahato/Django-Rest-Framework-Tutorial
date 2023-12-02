d6c647ed2aebcc4c5b536ac480f413c647401973
Jayesh <Jayesh-Mahato@users.noreply.github.com>
2023-12-02 17:24:43 +0530

    Update basic.py

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,12 @@
 import requests
 
 # endpoint = "https://httpbin.org/status/200/"
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://localhost:8000/"
 
 # HTTP Request
 get_response = requests.get(endpoint, json={"query:":"Hello World"}) 
-get_response2 = requests.get(endpoint, data={"query:":"Hello World"}) 
+# get_response2 = requests.get(endpoint, data={"query:":"Hello World"}) 
 # print(get_response.text) # print raw text response
 
 # print(get_response.json())
