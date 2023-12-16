00dec9d98aa4488a5a195b131ac0ca882a98ecf7
Jayesh <jayesh@virtualinit.net>
2023-12-16 19:14:11 +0530

    ingest data with DRF views

--- py_client/basic.py
+++ py_client/basic.py
@@ -8,7 +8,8 @@ endpoint = "http://localhost:8000/api/"
 # get_response = requests.get(endpoint, params={"abc": 123}, json={"query:":"Hello World"}) 
 # get_response2 = requests.get(endpoint, data={"query:":"Hello World"}) 
 # print(get_response.text) # print raw text response
-get_response = requests.get(endpoint, json={"product_id:": 123})
+# get_response = requests.get(endpoint, json={"product_id:": 123})
+get_response = requests.post(endpoint, json={"title": "Prod 36", "content": "Hello World", "price": "123"})
 
 # print(get_response.json())
 # print(get_response.status_code)
