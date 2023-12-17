5a538a48b9dc3d78afd99153aaf2b9e69b2933ba
Jayesh <jayesh@virtualinit.net>
2023-12-17 20:50:07 +0530

    viewsets & routers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -7,6 +7,7 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
+            'pk',
             'title',
             'content',
             'price',
