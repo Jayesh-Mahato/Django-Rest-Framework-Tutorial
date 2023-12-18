9953fe8f5d994383aa0d9fb073b949b06f155a02
Jayesh <jayesh@virtualinit.net>
2023-12-18 21:26:26 +0530

    urls reverse

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 # /api/products
 urlpatterns = [
-    path('', views.product_list_create_view),
+    path('', views.product_list_create_view, name='product-list'),
     # path('', views.product_mixin_view),
-    path('<int:pk>/update/', views.product_update_view),
+    path('<int:pk>/update/', views.product_update_view, name='product-edit'),
     path('<int:pk>/delete/', views.product_destroy_view),
-    path('<int:pk>/', views.product_detail_view),
+    path('<int:pk>/', views.product_detail_view, name='product-detail'),
     # path('<int:pk>/', views.product_mixin_view)
 
     # path('', views.product_alt_view),
